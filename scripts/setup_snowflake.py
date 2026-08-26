5a8a1156515732347407300fc1905646c5b810d8
sreekar-meruva <sreekar.meruva@gmail.com>
2026-08-26 00:23:48 -0500

    Snowflake setup fix

--- scripts/setup_snowflake.py
+++ scripts/setup_snowflake.py
@@ -19,18 +19,5 @@ def run_ddl():
     cursor.close()
     connection.close()
 
-def rewrite_performance_log():
-    connection = get_snowflake_connection()
-    cursor = connection.cursor()
-    file_paths = [r"sql\ddl\004_performance_log.sql",r"sql\ddl\002_actuals.sql"]
-    for file_path in file_paths:
-        with open(file_path) as f:
-            sql_script = f.read()
-        cursor.execute(sql_script)
-        print("Execution successful!")
-    cursor.close()
-    connection.close()
-
 if __name__=="__main__":
-    rewrite_performance_log()
-    # run_ddl()
+    run_ddl()
