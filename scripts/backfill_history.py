51d4d20f72e26212060303ee1d729c145b8d7b03
sreekar-meruva <sreekar.meruva@gmail.com>
2026-08-28 06:51:40 -0500

    UPDATE: Update backfill history to drift_log

--- scripts/backfill_history.py
+++ scripts/backfill_history.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import json
 import joblib
-from src.score_and_monitor import write_performance_log
+from src.score_and_monitor import write_performance_log, write_drift_log
 from src.score_and_monitor import get_performance_metrics
 from src.validate_drift_detection import get_model_predictions, alert_log, get_drift_scores
 from scripts.setup_snowflake import get_snowflake_connection
 import datetime
 
-def backfill_performance(model, metadata):
+def backfill_logs(model, metadata):
     df = pd.read_csv(r"data\processed\Production_prepared_stream.csv")
     connection = None
     cursor = None
     try:
         connection = get_snowflake_connection()
         cursor = connection.cursor()
-        UNIQUE_WEEKS_SCRIPT = f"SELECT DISTINCT WEEK FROM PERFORMANCE_LOG"
+        UNIQUE_WEEKS_SCRIPT = f"SELECT DISTINCT WEEK FROM DRIFT_LOG"
         cursor.execute(UNIQUE_WEEKS_SCRIPT)
         db_weeks = [week[0] for week in cursor.fetchall()]
         df_weeks = df['Week'].unique()
@@ -25,7 +25,10 @@ def backfill_performance(model, metadata):
             if week in db_weeks:
                 continue
             df_data = df.loc[(df['Week']==week)]
+
             drift_scores = get_drift_scores(baseline_stats=metadata['baseline_stats'],prod_stream=df_data, features=cont_features)
+            write_drift_log(metadata['baseline_stats'],features=cont_features,drift_scores=drift_scores['drift_scores'],weekly_mean=drift_scores['weekly_means'],week=week)
+
             pred_df = get_model_predictions(model, threshold, df_data, features)
             initial_count = len(pred_df)
             df_data = df_data.dropna(subset='Churn')
@@ -48,7 +51,7 @@ def backfill_performance(model, metadata):
                 'Week_not_null_count': current_count
             }
 
-            write_performance_log(report, metadata)
+            # write_performance_log(report, metadata)
 
     except Exception as e:
         raise Exception(f"Unable to backfill to table PERFORMANCE_LOG due to {e}")
@@ -61,4 +64,5 @@ if __name__=="__main__":
     with open(r"artifacts\model_metadata.json",'r') as f:
         metadata = json.load(f)
     model = joblib.load(r"artifacts\RandomForest.joblib")
-    backfill_performance(model, metadata)
+    
+    backfill_logs(model, metadata)
