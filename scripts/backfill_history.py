7e16862d77be2c7811282e68b57f14d66eda1859
sreekar-meruva <sreekar.meruva@gmail.com>
2026-08-28 06:59:47 -0500

    UPDATE: Updating actuals backfill history

--- scripts/backfill_history.py
+++ scripts/backfill_history.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import json
 import joblib
-from src.score_and_monitor import write_performance_log, write_drift_log
+from src.score_and_monitor import write_performance_log, write_drift_log, write_actuals
 from src.score_and_monitor import get_performance_metrics
 from src.validate_drift_detection import get_model_predictions, alert_log, get_drift_scores
 from scripts.setup_snowflake import get_snowflake_connection
@@ -14,7 +14,7 @@ def backfill_logs(model, metadata):
     try:
         connection = get_snowflake_connection()
         cursor = connection.cursor()
-        UNIQUE_WEEKS_SCRIPT = f"SELECT DISTINCT WEEK FROM DRIFT_LOG"
+        UNIQUE_WEEKS_SCRIPT = f"SELECT DISTINCT WEEK FROM ACTUALS"
         cursor.execute(UNIQUE_WEEKS_SCRIPT)
         db_weeks = [week[0] for week in cursor.fetchall()]
         df_weeks = df['Week'].unique()
@@ -25,6 +25,7 @@ def backfill_logs(model, metadata):
             if week in db_weeks:
                 continue
             df_data = df.loc[(df['Week']==week)]
+            write_actuals(df_data)
 
             drift_scores = get_drift_scores(baseline_stats=metadata['baseline_stats'],prod_stream=df_data, features=cont_features)
             write_drift_log(metadata['baseline_stats'],features=cont_features,drift_scores=drift_scores['drift_scores'],weekly_mean=drift_scores['weekly_means'],week=week)
@@ -51,7 +52,7 @@ def backfill_logs(model, metadata):
                 'Week_not_null_count': current_count
             }
 
-            # write_performance_log(report, metadata)
+            write_performance_log(report, metadata)
 
     except Exception as e:
         raise Exception(f"Unable to backfill to table PERFORMANCE_LOG due to {e}")
